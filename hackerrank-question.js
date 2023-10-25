736e8b1e7f970ef7f04aee00bff82666a44f1fed
Oladimeji Tobi <tobioladimeji123@gmail.com>
2023-10-25 20:35:24 +0100

    Update hackerrank-question.js

--- hackerrank-question.js
+++ hackerrank-question.js
@@ -1,4 +1,5 @@
 /*
+Question 1
 Complete the function solveMeFirst to compute the sum of two integers.
 
 Example
@@ -25,3 +26,35 @@ function solveMeFirst(a, b) {
   // Hint: Type return a+b below
   return a + b;
 }
+
+/*
+Question 2
+
+Given an array of integers, find the sum of its elements.
+
+For example, if the array , , so return .
+
+Function Description
+
+Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+simpleArraySum has the following parameter(s):
+
+ar: an array of integers
+Input Format
+
+The first line contains an integer, , denoting the size of the array.
+The second line contains  space-separated integers representing the array's elements.
+*/
+
+function simpleArraySum(ar) {
+    // Write your code here
+    let sum = 0;
+    for (const num of ar) {
+        sum += num;
+    }
+    
+    return sum;
+}
+
+
